8e946ddc1c43f66e55aa56f357923228bdab0d0b
Pranav <pguru12@gmail.com>
2017-06-05 19:02:43 -0400

    Trying to add another input to the form 2

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ function handleSubmit(ev){
     const f = ev.target
     const heading=document.querySelector('h1')
     heading.textContent=f.personName.value
+    
+}
+
+function handleSubmitTwo(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const heading=document.querySelector('h1')
     heading.textContent=f.personAge.value
 }
 
 personForm.addEventListener('submit', handleSubmit)
+personForm.addEventListener('submit', handleSubmitTwo)
